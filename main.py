a7c9e38be387554ab81932164b2c5c2e3cdc263d
ange-Nguetsop <nguetsopange@yahoo.com>
2024-01-30 16:43:44 +0100

    Bilder von Zwischenergebnisse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-image = cv2.imread('Tomate3.jpg')
+image = cv2.imread('TomatesAvo.jpg')
 result = image.copy()
 image = cv2.GaussianBlur(image,(11,11),0)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -9,6 +9,7 @@ lower = np.array([0,77,42])
 upper = np.array([18,255,255])
 mask = cv2.inRange(image, lower, upper)
 output = cv2.bitwise_and(result,result, mask= mask)
+cv2.imwrite('zwischenErgebnis.png',output)
 output = cv2.cvtColor(output,cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(output,30,255,cv2.THRESH_BINARY)
     
@@ -23,6 +24,7 @@ dilated = cv2.dilate(thresh, kernel1, iterations=4)  # Augmenter les iterations 
     #eroded = cv2.erode(dilated, kernel, iterations=4)  # Ajuster les iterations selon les besoins
 kernel2 = np.ones((9,9), np.uint8)  # La taille du noyau affecte le degré de fusion
 eroded = cv2.erode(dilated, kernel2, iterations= 5)
+cv2.imwrite('zwichen2.png',eroded)
 
 contours, hierarchy = cv2.findContours(eroded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -56,7 +58,6 @@ for i in range (0,len(index)):
         center.append([cx,cy])
 
 radius = []
-
 for i in range(0,len(center)):
     r = (center[i][0]-topmost[i][0]) ** 2 + (center[i][1]-topmost[i][1]) ** 2
     r = r ** (1/2)
@@ -66,6 +67,7 @@ for i in range(0,len(center)):
         # Parcourir tous les contours trouvés
 for i in range (0,len(center)):
     cv2.circle(img = result, center = center[i], radius = radius[i] + 10, color =  (0, 255, 0) , thickness = 5, lineType = cv2.LINE_AA)
+
 cv2.imwrite('result2.png',result)
 cv2.imshow('result', result)
 
